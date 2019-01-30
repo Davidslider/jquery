a74d8d78b4d3403cf5e97da9eed60ccb0d2cb82f
Davidslider <46541772+Davidslider@users.noreply.github.com>
2019-01-30 17:24:34 -0600

    finsihed product

---
+++ js/main.js
@@ -0,0 +1,22 @@
+//step one: collect input from input tag
+//step two: we need to add funtionality to the button when clicked/
+$('button').on('click', createSkill)
+//step three: Once button clicked we need to create a skill element with a delete button
+function createSkill(){
+   const $skill = $('input').val()
+   if(!$skill){
+       alert('YO! Chill add a skill')
+   } else {
+   const $listItem = $(`<p><span>X</span> | ${$skill}</p>`)
+   
+
+   //step four: once skill is created we need to append the DOM 
+   $('h1').after($listItem)
+   $('input').val('')
+   $('span').on('click', function(){
+       $(this).closest('p').remove()
+   })
+  }   
+}
+
+
